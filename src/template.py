7c59e243607eda2750a443690b10ce56814ac519
Shamus <scliff108@gmail.com>
2023-12-22 17:45:14 -0500

    Update Template

--- src/template.py
+++ src/template.py
@@ -1,17 +1,20 @@
 DAY = 0
 FILENAME = 'test.txt'
 
+
 def part_one(filename):
     with open(filename, 'r') as f:
         for line in f:
             print(line)
     return ''
 
+
 def part_two(filename):
     with open(filename, 'r') as f:
         for line in f:
             print(line)
     return ''
+
 
 def main():
 
@@ -24,6 +27,7 @@ def main():
     print('Part 2')
     print(part_two(f'data/day{DAY}/{FILENAME}'))
     '''
+
 
 if __name__ == '__main__':
     main()
